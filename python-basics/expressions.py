af7b4abcc2c466e35ff6bd52a8292e490bb7e63a
Naufal Yoga Pratama <129613027+NaufalYogaPratama@users.noreply.github.com>
2025-01-09 23:14:15 +0700

    Types of Operators

--- python-basics/expressions.py
+++ python-basics/expressions.py
@@ -16,3 +16,69 @@ print(-d)
 print(2+2)
 print(3<10)
 print(True or False)
+
+
+# Jenis-jenis Operator
+# 1. Operator Aritmatika
+aritx = 11
+arity = 5
+print(aritx + arity)
+print(aritx - arity)
+print(aritx * arity)
+print(aritx // arity)
+print(aritx / arity)
+print(aritx % arity)
+print(aritx ** arity)
+
+# 2. Operator Relational
+relx = 5
+rely = 10
+print(relx == rely)
+print(relx != rely)
+print(relx > rely)
+print(relx < rely)
+print(relx >= rely)
+print(relx <= rely)
+
+# 3. Operator Logika
+# And
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+# Or
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+# Not
+print(not True)
+print(not False)
+
+# 4. Operator Assignment
+# +=
+x = 11
+x += 5
+print(x)
+
+# -=
+x = 11
+x -= 5
+print(x)
+
+# *=
+x = 11
+x *= 5
+print(x)
+
+# /=
+x = 11
+x /= 5
+print(x)
+
+#%=
+x = 11
+x%= 5
+print(x)
