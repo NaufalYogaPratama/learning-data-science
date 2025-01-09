6b4e60c6bb9ba4bd6d1d71bb71bdce26438874b6
Naufal Yoga Pratama <129613027+NaufalYogaPratama@users.noreply.github.com>
2025-01-09 21:38:09 +0700

    i/o in python

---
+++ python-basics/learn.py
@@ -0,0 +1,12 @@
+"""
+This module demonstrates a simple example of a variable definition.
+"""
+
+name = input('Masukan nama Anda: ')
+print(name)
+
+# comment in py
+"""
+Ini adalah Block Comment,
+Teks ini akan diabaikan oleh Python.
+"""
