1c94341d88db784d04b16129bf7e08b3e5bdd342
Naufal Yoga Pratama <129613027+NaufalYogaPratama@users.noreply.github.com>
2025-01-09 23:07:05 +0700

    Types of Expressions

---
+++ python-basics/expressions.py
@@ -0,0 +1,18 @@
+# Jenis-jenis Ekspresi
+# Ekspresi Menurut Arity dari Operator
+a = True
+a = not a
+print(a)
+b = 6
+b -= 1
+print(b)
+c = 6
+c += 1
+print(c)
+d = 10
+print(-d)
+
+# Ekspresi Menurut Tipe Data yang Dihasilkan
+print(2+2)
+print(3<10)
+print(True or False)
