1e825f808a860ea11140eb1cdfea78dc8e46686d
Naufal Yoga Pratama <129613027+NaufalYogaPratama@users.noreply.github.com>
2025-01-11 22:40:42 +0700

    Style guide using flake8 and black

---
+++ python-basics/styleGuide.py
@@ -0,0 +1,12 @@
+# Checked the style guide using flake8 and formatted using Black
+class Kalkulator:
+    """kalkulator tambah kurang"""
+
+    def __init__(self, _i):
+        self.i = _i
+
+    def tambah(self, _i):
+        return self.i + _i
+
+    def kurang(self, _i):
+        return self.i - _i
